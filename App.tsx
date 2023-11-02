96241e0694119046f339536178b7a4b7a1cb25d0
José María Soto valenzuela <116988804+Gorchon@users.noreply.github.com>
2023-11-02 11:40:49 -0600

    play reader done

--- App.tsx
+++ App.tsx
@@ -19,11 +19,24 @@ Audio.setAudioModeAsync({
 export default function App() { 
   const[borderColor, setBorderColor] = useState<"lightgray" | "lightgreen">("lightgray");
   const {state, startRecognizing, stopRecognizing, destroyRecognizing} = useVoiceRecognition();
+  const [urlPath, setUrlPath] = useState(""); //this is the path of the audio file that we are gonna play
+  const listFiles = async () => {
+    try{
+      const result = await FileSystem.readDirectoryAsync(FileSystem.documentDirectory!); //this is gonna return a promise
+      if(result.length > 0){
+        const filename = result[0];
+        const path = FileSystem.documentDirectory + filename;
+        setUrlPath(path);
+      }
+    }catch(e){
+      console.log(e);
+    }
+  }
   const handleSubmit = async () => {
     if(!state.results[0])return; 
     try{
       //fetch the audio and then  this is gonna be a blob, bynary large object. From the server 
-      const audioBlob = await fetchAudio(state.results[0]);
+      const audioBlob = await fetchAudio(state.results[0]); // blomb means that it's a file that we don't know what it is  
 
       const  reader = new FileReader(); // file reader is a browser api that allows us to read files
       reader.onload = async (e) => {
@@ -32,6 +45,7 @@ export default function App() {
           // save data  
           const path = await writeAudioToFile(audioData);  //this return a promise
           //play the audio
+          setUrlPath(path);
           await playFromPath(path);
           destroyRecognizing();
         }
@@ -65,7 +79,7 @@ export default function App() {
          onPressOut={() => {
           setBorderColor("lightgray");
           stopRecognizing();
-          //handleSubmit();
+          handleSubmit();
          }}
          style = {{ 
           width: "90%",
@@ -80,7 +94,9 @@ export default function App() {
           <Text>Hold to speak</Text>
          </Pressable>
          <Text style = {{marginVertical: 10, fontSize: 17}}>{JSON.stringify(state, null, 2)}</Text>  
-         <Button title = "Reply last message"  onPress={() => {}}/>
+         <Button title = "Reply last message"  onPress={async() => {
+          await playFromPath(urlPath); 
+         }}/>
     </View>
   );
 }
