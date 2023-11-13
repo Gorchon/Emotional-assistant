ac89cad8e0e4890583fa54a43e736d3c7362fc0a
José María Soto valenzuela <116988804+Gorchon@users.noreply.github.com>
2023-11-13 15:40:39 -0600

    fixing my errors

--- utils /playFromPath.ts
+++ utils /playFromPath.ts
@@ -2,7 +2,7 @@ import { Audio } from 'expo-av';
 export const playFromPath = async (path:string) => {
     try{
         const soundObject = new Audio.Sound();
-        await soundObject.loadAsync({uri: path});
+        await soundObject.loadAsync({uri: path}); // here the loadAsync method is expecting an object with a uri property that is a string with the path to the audio file
         await soundObject.playAsync();
     }
     catch(error){
